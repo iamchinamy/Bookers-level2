228d4c53cf9472f033aae6bce5037d7e7e54f678
Chinami Kitagawa <cnm25.k@gmail.com>
2020-08-03 18:55:03 +0900

    first commit 課題3

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'a22b5711695c8a61c17b4ac92d14c66817ceae553d0803131e68586750b7dac9e3738e729bc15e8de49fc4e51bdfe59a0b2ef91316f032db098441cfe3e8dce1'
